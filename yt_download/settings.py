4e5501365897b8b2e412515b0fbb5d2566cdddb2
AbdulrahmanSamir01 <s.abdulrahman.a.samir.2022@gmail.com>
2025-05-23 16:28:43 +0300

    production mode

--- yt_download/settings.py
+++ yt_download/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yf*k&y_rf+7st*$-#hto(o!ufb^!4w%4z&tnweef!nfr^$^-m2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
