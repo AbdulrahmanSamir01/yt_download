da24cbabf4fc9ed3b0a0b1a38718d858a4830f9b
AbdulrahmanSamir01 <s.abdulrahman.a.samir.2022@gmail.com>
2025-05-24 17:36:21 +0300

    chunk-by-chunk

--- dl_app/urls.py
+++ dl_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.download_video, name='download_video'),
+    path('', views.index, name='index'),
+    path('stream/', views.stream_video, name='stream_video'),
 ]
